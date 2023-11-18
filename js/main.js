c341ff9a91c6ff22ae5c380766367646286bf206
elrate99 <vadim300012@gmail.com>
2023-11-18 03:12:24 +0200

    big update
    add sign in and sign up forms and style with css and js;
    added new fonts and icons
    added js framework

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ const navPanel = document.getElementById('nav-panel');
 const over = document.querySelector('.overlay');
 const modalWrapper = document.querySelector('.modal-wrapper');
 const userPanel = document.getElementById('user-panel');
+const inputAuth = document.querySelector('.input100');
+const signUpLink = document.querySelector(".txt2");
+const signInLink = document.querySelector(".txt3");
+const signInAuth = document.querySelector(".auth-popup");
+const signUpAuth = document.querySelector(".sign-popup");
 
 menuBtn.addEventListener("click", () => {
     navPanel.style.display = 'flex';
@@ -19,4 +24,16 @@ over.addEventListener("click", () => {
 
 userPanel.addEventListener("click", () => {
     modalWrapper.style.display = "flex";
+})
+
+signUpLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    signInAuth.style.display = 'none';
+    signUpAuth.style.display = "block";
+})
+
+signInLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    signUpAuth.style.display = 'none';
+    signInAuth.style.display = "block";
 })
