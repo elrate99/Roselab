5bfcf58e3c7c7ebde560bc30da3e45ac5017a2ec
elrate99 <vadim300012@gmail.com>
2023-11-21 01:38:58 +0200

    fix bugs, add faq page

--- js/main.js
+++ js/main.js
@@ -10,6 +10,12 @@ const signInLink = document.querySelector(".txt3");
 const signInAuth = document.querySelector(".auth-popup");
 const signUpAuth = document.querySelector(".sign-popup");
 
+// FAQ page
+const faqBtns = document.querySelectorAll(".faq-fi");
+const faqBodys = document.querySelectorAll(".faq-body");
+const faqBtn = document.querySelector(".faq-fi");
+const faqBody = document.querySelector(".faq-body");
+
 menuBtn.addEventListener("click", () => {
     navPanel.style.display = 'flex';
 })
@@ -36,4 +42,18 @@ signInLink.addEventListener("click", (e) => {
     e.preventDefault();
     signUpAuth.style.display = 'none';
     signInAuth.style.display = "block";
+})
+
+faqBtns.forEach(faqBtn => {
+        faqBtn.addEventListener("click", () => {
+            faqBodys.forEach(faqBody => {
+                if (faqBody.style.display == "none") {
+                    faqBtn.style.transform = "rotate(90deg)";
+                    faqBody.style.display = "block";
+            } else {
+                    faqBtn.style.transform = "rotate(45deg)";
+                    faqBody.style.display = "none";
+            }
+            })
+    })
 })
